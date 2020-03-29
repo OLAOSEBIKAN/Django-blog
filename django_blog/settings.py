1cd551cf892967dc3b4c4fea55f8fb771d8a5df1
olaitan <olaonipekunolaitan@gmail.com>
2020-03-29 14:51:53 +0100

    updated settings, config

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l5_tj$vnoe(6y_*3pi+i!69sy29k9yk!8s(cm%b3g$nlp40b@9'
+#SECRET_KEY = 'l5_tj$vnoe(6y_*3pi+i!69sy29k9yk!8s(cm%b3g$nlp40b@9'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["djangothought.herokuapp.com"]
 
 
 # Application definition
