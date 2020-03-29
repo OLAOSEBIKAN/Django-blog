87597dca8a4a49e2c534c35eb1fa839c67e39af3
olaitan <olaonipekunolaitan@gmail.com>
2020-03-29 16:49:53 +0100

    bootstrap setup

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'users',
     'storages',
     'crispy_forms',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
