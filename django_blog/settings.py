8e1beec0560422d2fb95c7b3345a997fff5dc032
olaitan <olaonipekunolaitan@gmail.com>
2020-04-05 23:26:34 +0100

    temp

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -153,11 +153,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-STATICFILES_DIRS = (
+'''STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+)'''
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
