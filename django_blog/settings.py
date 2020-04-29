8f0226cb2ca5cafe05772980b49ee20fc5474481
olaitan <olaonipekunolaitan@gmail.com>
2020-04-29 05:27:15 +0100

    added heroku settings

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 #DEBUG = True
 
-ALLOWED_HOSTS = ["djangothought.herokuapp.com",]
+ALLOWED_HOSTS = ["djangothought.herokuapp.com"]
 
 
 # Application definition
@@ -181,18 +181,3 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
 
-# Heroku settings
-if os.getcwd() == '/app':
-    import dj_database_url
-    DATABASES = {
-           'default': dj_database_url.config(default='postgres://localhost')
-    }
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure().
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https') # Allow all host headers.
-    ALLOWED_HOSTS = ['*']
-       # Static asset configuration
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
-        os.path.join(BASE_DIR, 'static'),
-    )
